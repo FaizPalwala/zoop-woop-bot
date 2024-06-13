0de657316a87e85c8d9d6b5fb5c3fc0ce33acbc2
fpalwala <faiz.palwala@gmail.com>
2024-06-13 10:14:29 +0530

    DB Utility initial commit

---
+++ DBUtil.py
@@ -0,0 +1,44 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv()
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
+
+def add_subscriptions(subs):
+    data, count = supabase.table('subscriptions').upsert(subs).execute()
+    return data
+
+def get_subscriptions():
+    data, count = supabase.table('subscriptions').select('*').execute()
+    return data[1]
+
+def get_listing_to_send(geo_id, chat_id, ignore_listings):
+    return supabase.rpc(fn='get_listings_to_send',
+                        params={
+                            'curr_geo_id' : geo_id,
+                            'curr_chat' : chat_id,
+                            'ignore_listings' : ignore_listings
+                        }).select('*').limit(5).execute().data
+
+def get_listing_param():
+    return supabase.rpc('get_listing_param').select('*').limit(1).execute()
+
+def add_listings(listings):
+    data, count = supabase.table('listings').upsert(listings).execute()
+    return data
+
+def get_listing_by_geo_id(geo_id):
+    data, count = supabase.table('listings').select('*').eq('geo_id', geo_id).execute()
+    return data[1]
+
+def create_transaction(transaction):
+    data, count = supabase.table('transactions').upsert(transaction).execute()
+    return data
+
+
+
