f7baeacd7856ddb3949d8482531c7e7e91e29424
fpalwala <faiz.palwala@gmail.com>
2024-06-13 11:40:00 +0530

    replaced not in with != any(), compliant with plpgsql

--- resources/get_listings_to_send.sql
+++ resources/get_listings_to_send.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION get_listings_to_send(curr_geo_id VARCHAR, curr_chat VARCHAR, ignore_listings VARCHAR[])
   RETURNS SETOF listings AS $$
   BEGIN
-    RETURN QUERY SELECT * FROM listings WHERE geo_id = curr_geo_id AND listing_id NOT IN (SELECT listing_id FROM transactions WHERE chat_id = curr_chat AND listing_id != any(ignore_listings));
+    RETURN QUERY SELECT * FROM listings WHERE geo_id = curr_geo_id AND listing_id != any(SELECT listing_id FROM transactions WHERE chat_id = curr_chat AND listing_id != any(ignore_listings));
   END;
 $$ LANGUAGE plpgsql;
